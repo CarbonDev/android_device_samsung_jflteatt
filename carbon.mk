4414b9a4acb548d3f13ac505b940d4729334c6d3
nocoast <voteforpedro8@gmail.com>
2013-06-01 18:34:50 -0500

    Another to the list

--- carbon.mk
+++ carbon.mk
@@ -1,16 +1,13 @@
 $(call inherit-product, device/samsung/jflteatt/full_jflteatt.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/carbon/config/common_gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteuc TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.2.2/JDQ39/I337UCUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jflteuc-user 4.2.2 JDQ39 I337UCUAMDL release-keys"
 
-PRODUCT_NAME := cm_jflteatt
+PRODUCT_NAME := carbon_jflteatt
 PRODUCT_DEVICE := jflteatt
 
